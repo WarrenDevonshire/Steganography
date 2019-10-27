5560b8220193edfa96efbbb485687edafee77fc5
ChrisMalitsky <cjmalitsky@gmail.com>
2019-10-27 15:55:47 -0400

    removed unneeded code

--- src/image_construction.py
+++ src/image_construction.py
@@ -25,18 +25,7 @@ def to_image(arr, img_shape):
     return new_img
 
 def main():
-    # #carry_arr = to_array(CARRY_IMAGE_FILE)
-    # mesg = Image.open(MESG_IMAGE_FILE)
-
-    # mesg_arr_pixels = list(mesg.getdata())
-    # #print(mesg_arr_pixels)
-    # #mesg_arr = np.array(mesg_arr_pixels, dtype=np.uint8)
-
-    # new_img = Image.new('1', (mesg.size[0], mesg.size[1]))
-
-    # new_img.putdata(mesg_arr_pixels)
-
-    # #reconstruct_image = to_image(mesg_arr_pixels, mesg.size[0], mesg.size[1])
+    
     mesg = MESG_IMAGE_FILE 
 
     mesg_arr = to_array(mesg)
