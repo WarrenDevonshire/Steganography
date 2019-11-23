5b2f75bdabf25c05370dcb03df3f80c34c977b5c
warrendevonshire <wdevonshire@icloud.com>
2019-11-23 14:53:48 -0500

    Update test_compression_strategy.py

--- steganography/tests/test_compression_strategy.py
+++ steganography/tests/test_compression_strategy.py
@@ -9,24 +9,10 @@ SECRET_IMAGE_PATH = "../resources/basi0g01.png"
 
 class TestCompressionStrategy(unittest.TestCase):
 
-    def test_numpy_to_bits(self):
-        with open(SECRET_IMAGE_PATH, 'rb') as file:
-            data = file.read()
-            byte_array = np.frombuffer(data, dtype='uint8')
-            data_str = []
-            for byte in data:
-                data_str.append(byte)
-            print(data_str)
-            print(byte_array)
-
-            bits = np.unpackbits(byte_array)
-            print(np.packbits(bits))
-
     def test_compression_strategy(self):
         with Image.open(CARRY_IMAGE_PATH) as image:
             image = np.array(image)
 
-        shape = image.shape
         image = image.ravel()
 
         with open(SECRET_IMAGE_PATH, 'rb') as file:
