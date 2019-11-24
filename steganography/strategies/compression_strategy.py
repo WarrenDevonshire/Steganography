6c20cdcc1fd9af47a424e60c5317954d71e9b49d
warrendevonshire <wdevonshire@icloud.com>
2019-11-23 19:05:40 -0500

    refactor lsb

--- steganography/strategies/compression_strategy.py
+++ steganography/strategies/compression_strategy.py
@@ -1,49 +1,7 @@
+from steganography.strategies.lsb import hide_data_in_LSB, get_data_in_LSB
 from steganography.utilities.rand import get_random_sequence
 from steganography.utilities.compression import compress_data, decompress_data
 import numpy as np
-
-# TODO: convert to class
-
-
-def hide_data_in_LSB(carry, data, seed=0):
-    """Hide data in carry and return a numpy array.
-
-    Keyword arguments:
-    carry -- the one dimensional array to hide data in
-    data -- an array of bits to hide
-    seed -- an integer (default = 0)
-    """
-    upper_bound = (len(carry) // 8) * 8
-    data_size = len(data)
-
-    assert(data_size < upper_bound)
-
-    seq = get_random_sequence(0, upper_bound, seed)
-
-    for index, bit in enumerate(data):
-        random_index = seq[index]
-        bit_mask = 1
-        byte = carry[random_index] & ~bit_mask
-        if bit:
-            byte = byte | bit_mask
-        carry[random_index] = byte
-
-    return carry
-
-
-def get_data_in_LSB(carry, seed=0):
-    upper_bound = (len(carry) // 8) * 8
-
-    seq = get_random_sequence(0, upper_bound, seed)
-
-    bits = np.empty(upper_bound, dtype='uint8')
-
-    # for index, byte in enumerate(carry):
-    #     bits[index] = byte % 2
-    for index, random_index in enumerate(seq):
-        bits[index] = carry[random_index] % 2
-
-    return np.packbits(bits)
 
 
 def hide_compressed_data(carry, data, seed=0):
