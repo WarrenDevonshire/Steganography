d9155902639b295735e74732caa00533821aedad
warrendevonshire <wdevonshire@icloud.com>
2019-12-01 17:09:28 -0500

    added blue strategy.

---
+++ steganography/strategies/blue.py
@@ -0,0 +1,30 @@
+import numpy as np
+from steganography.utilities.rand import get_random_sequence
+import logging
+
+SIZE_LENGTH = 32
+
+# 24 bit pixel like:
+# 3 MSB --> Blue LSB
+# 2 MSB --> Green LSB
+# 1 MSB --> Red LSB
+
+
+def hide_data_in_blue(carry, data, seed):
+    """
+
+    :param carry:
+    :param data:
+    :param seed:
+    :return:
+    """
+    upper_bound = (len(carry) // 8) * 8
+    data_size = len(data)
+
+    logging.info("HIDE_DATA: Data size: %s", data_size // 8)
+    carry_data_limit = (upper_bound * 6) // 8
+    logging.info("HIDE_DATA: Carry limit is: %s bytes", carry_data_limit)
+    logging.info("HIDE_DATA: Carry limit is: %s kilobytes", carry_data_limit / 1_000)
+    logging.info("HIDE_DATA: Carry limit is: %s megabytes", carry_data_limit / 1_000_000)
+
+
--- steganography/tests/test_blue.py
+++ steganography/tests/test_blue.py
@@ -1,8 +1,8 @@
 import unittest
+import logging
 from PIL import Image
 import numpy as np
-
-from steganography.strategies.lsb import hide_data_in_LSB, get_data_in_LSB
+from steganography.strategies.blue import hide_data_in_blue
 from steganography.utilities.aes import generate_key_and_seed, encrypt, decrypt
 
 IMAGE = "../resources/hello.png"
@@ -10,9 +10,10 @@ FILE = "../resources/basi0g01.png"
 PASSCODE = "Hello, World!"
 
 
-class TestLsb(unittest.TestCase):
+class TestBlue(unittest.TestCase):
 
-    def test_lsb(self):
+    def test_hide_data_in_blue(self):
+        logging.basicConfig(filename='TestBlue.log', level=logging.INFO, format='%(asctime)s %(message)s')
         with Image.open(IMAGE) as image, open(FILE, 'rb') as file:
             image = np.array(image).ravel()
             data = file.read()
@@ -25,12 +26,12 @@ class TestLsb(unittest.TestCase):
 
         data = np.unpackbits(data)
 
-        carry = hide_data_in_LSB(image, data, seed)
+        carry = hide_data_in_blue(image, data, seed)
 
-        key, seed = generate_key_and_seed(PASSCODE)
-        data2 = get_data_in_LSB(carry, seed)
-
-        data2 = data2.tobytes()
-        data2 = decrypt(data2, key)
-
-        assert(data0 == data2)
+        # key, seed = generate_key_and_seed(PASSCODE)
+        # data2 = get_data_in_LSB(carry, seed)
+        #
+        # data2 = data2.tobytes()
+        # data2 = decrypt(data2, key)
+        #
+        # assert(data0 == data2)
