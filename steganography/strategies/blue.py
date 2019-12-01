b6449b96187af1c2d359ee62b113d443195fbb27
warrendevonshire <wdevonshire@icloud.com>
2019-12-01 18:56:20 -0500

    Update blue.py

--- steganography/strategies/blue.py
+++ steganography/strategies/blue.py
@@ -18,13 +18,19 @@ def hide_data_in_blue(carry, data, seed):
     :param seed:
     :return:
     """
-    upper_bound = (len(carry) // 8) * 8
+    upper_bound = (len(carry))
     data_size = len(data)
 
-    logging.info("HIDE_DATA: Data size: %s", data_size // 8)
-    carry_data_limit = (upper_bound * 6) // 8
+    logging.info("HIDE_DATA: Data size: %s", data_size / 8)
+    logging.info("HIDE_DATA: Data size multiple of 6: %s", data_size / 6)
+    carry_data_limit = (upper_bound * 6) / 8
+
     logging.info("HIDE_DATA: Carry limit is: %s bytes", carry_data_limit)
     logging.info("HIDE_DATA: Carry limit is: %s kilobytes", carry_data_limit / 1_000)
     logging.info("HIDE_DATA: Carry limit is: %s megabytes", carry_data_limit / 1_000_000)
 
+    assert(data_size < (upper_bound * 6))
 
+    compress_ratio = (float(carry_data_limit) - float(data_size // 8)) / float(carry_data_limit)
+
+    logging.info("Data takes up: %d%%" % (100.0 * compress_ratio))
