2e4da7bb0adad3fdb372e4704ba7e65057c649f1
warrendevonshire <wdevonshire@icloud.com>
2019-12-01 16:23:09 -0500

    core refactor and tests.

---
+++ steganography/tests/test_core.py
@@ -0,0 +1,22 @@
+import unittest
+from steganography.core.core import Steganography
+from steganography.strategies.lsb import hide_data_in_LSB, get_data_in_LSB
+
+IMAGE = "../resources/hello.png"
+DATA = "../resources/basi0g01.png"
+OUT_PATH = "../out/test_hide_data.png"
+OUT_DATA_PATH = "../out/data.png"
+PASSCODE = "Hello, World!"
+
+
+class TestCore(unittest.TestCase):
+
+    def test_hide_data(self):
+        st = Steganography()
+        st.set_output_path(path_to_output=OUT_PATH)
+        st.hide_data(path_to_carry=IMAGE, path_to_secret=DATA, strategy=hide_data_in_LSB)
+
+    def test_get_data(self):
+        st = Steganography()
+        st.set_output_path(path_to_output=OUT_DATA_PATH)
+        st.get_data(path_to_carry=OUT_PATH, strategy=get_data_in_LSB)
