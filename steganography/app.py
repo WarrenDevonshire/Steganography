a15322d7280f1079d330eb2a5b0090552f2fd0c4
warrendevonshire <wdevonshire@icloud.com>
2019-12-09 00:08:11 -0500

    Update app.py

--- steganography/app.py
+++ steganography/app.py
@@ -10,23 +10,23 @@ GET_STRATEGIES = {
     "lsb": get_data_in_LSB
 }
 
-flags = {
-    "encrypt": False,
-    "decrypt": False,
-    "password": False,
-    "output_path": "./out",
-    "verbose": False,
+LOG_LEVELS = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warn': logging.WARN,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL
 }
 
 
-def setup_logger():
+def setup_logger(level):
     # create logger
     logger = logging.getLogger('steganography')
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(level)
 
-    # create console handler and set level to debug
+    # create console handler and set level
     ch = logging.StreamHandler()
-    ch.setLevel(level=logging.DEBUG)
+    ch.setLevel(level=level)
 
     # create formatter
     formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
@@ -41,46 +41,33 @@ def setup_logger():
 # TODO: Add flags for strategy selection
 # TODO: Remove unnecessary png files from resources.
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("carry", help="path to carry image")
-    parser.add_argument("secret", help="path to secret file to hide")
-    parser.add_argument("-d", "--decrypt", help="retrieve hidden data from image", action='store_true')
-    parser.add_argument("-o", "--output", help="path to store program output")
-    parser.add_argument("-p", "--passcode", help="passcode to encrypt data")
+    parser = argparse.ArgumentParser(prog='steganography')
+    parser.add_argument('--log-level', help='set logging level', choices=['debug', 'info', 'warn', 'error', 'critical'],
+                        default='info')
+    subparsers = parser.add_subparsers(help='help for subcommand', required=True, dest='subcommand')
+
+    # create parser for hide
+    parser_hide = subparsers.add_parser('hide', help='hide help')
+    parser_hide.add_argument('image', help='image to hide data in')
+    parser_hide.add_argument('data', help='data to hide')
+    parser_hide.add_argument('-o', '--output', help='path to store output image', default='./out.png')
+    parser_hide.add_argument('-e', '--encrypt', help='encrypt data',  action='store_true', default=False)
+    parser_hide.add_argument('-c', '--compression', help='compress data',  action='store_true', default=False)
+
+    # create parser for get
+    parser_get = subparsers.add_parser('get', help='get help')
+    parser_get.add_argument('image', help='image to get data from')
+    parser_get.add_argument('-o', '--output', help='path to store output data', default='./out')
+    parser_get.add_argument('-d', '--decrypt', help='decrypt data',  action='store_true', default=False)
+    parser_get.add_argument('-c', '--compression', help='decompress data',  action='store_true', default=False)
+
     args = parser.parse_args()
-    st = Steganography()
-    if args.passcode:
-        st.set_passcode(args.passcode)
-    if not args.decrypt:
-        st.set_output_path(args.output)
-        st.hide_data(args.carry, args.secret, hide_data_in_LSB)
-    else:
-        st.set_output_path(args.secret)
-        st.get_data(args.carry, get_data_in_LSB)
+
+    return args
 
 
 def run():
-    setup_logger()
-    parse_args()
+    args = parse_args()
+    setup_logger(LOG_LEVELS[args.log_level])
 
-
-class Steganography:
-
-    def __init__(self):
-        self.path_to_output = "./output.png"
-        self.passcode = ''
-
-    def set_output_path(self, path_to_output):
-        self.path_to_output = path_to_output
-
-    def set_passcode(self, passcode):
-        self.passcode = passcode
-
-    def hide_data(self, path_to_carry, path_to_secret, strategy):
-        st = Steganographer(path_to_carry)
-        # passcode = ''  # TODO: get passcode from stdin
-        st.hide_data_from_file(path=path_to_secret, out_path=self.path_to_output, strategy=strategy, passcode=self.passcode)
-
-    def get_data(self, path_to_carry, strategy):
-        # passcode = ''  # TODO: get passcode from stdin
-        Steganographer.get_data_from_image(image_path=path_to_carry, out_path=self.path_to_output, strategy=strategy, passcode=self.passcode)
+    log = logging.getLogger('steganography')
